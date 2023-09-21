4b75dcc387f791dea346233e9c4743460709b364
ngochung1809 <tongochung1809@gmail.com>
2023-09-21 17:17:44 +0700

    update jenkins

--- vars/apiGateway.groovy
+++ vars/apiGateway.groovy
@@ -1,24 +1,12 @@
 import devops.cicd.tools.Ssh
 
 def call() {
-    dockerNode(image: 'docker:latest') {
-        def ssh = new Ssh()
-        def serverDeployIP = "34.126.122.163"
-        def serverSonarqubeIP = "34.126.122.163"
-        def applicationDir = "/home/ubuntu/registry-service"
-        stage("Pull Code") {
-            ssh.executeCommand(serverDeployIP, executeDir(applicationDir, "git pull"), "ubuntu")
-        }
-
-        stage("Install Dependencies") {
-            ssh.executeCommand(serverDeployIP, executeDir(applicationDir, "mvn clean install"), "ubuntu")
-        }
-
-        stage("SonarQube Analysis") {
-            withSonarQubeEnv('SONARQUBE_SERVER') {
-                ssh.executeCommand(serverSonarqubeIP, executeDir(applicationDir, "mvn sonar:sonar"), "ubuntu")
-            }
-        }
+    def ssh = new Ssh()
+    def serverDeployIP = "34.126.122.163"
+    def serverSonarqubeIP = "34.126.122.163"
+    def applicationDir = "/home/ubuntu/registry-service"
+    stage("Pull Code") {
+        sh 'echo "Hello world"'
     }
 }
 
